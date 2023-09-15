9e2cd5c512bc1977e916495a025628111ed34dde
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-09-15 20:38:45 +0100

    Add files via upload

--- cd.sh
+++ cd.sh
@@ -11,5 +11,15 @@ cp *.elf CD_root/isolinux/
 cp *.jpg CD_root/isolinux/
 cp *.menu CD_root/isolinux/
 cp *.txt CD_root/isolinux/
+nasm -felf32 -o boot.o boot.S
+nasm -o hello.com hello.asm
+gcc -c kernel.c -o kernel.o -nostdlib
+gcc link.ld boot.o kernel.o -o kernel.elf -nostdlib
+nasm model.asm -o model.o
+objdump -D kernel.elf > out.txt
+cat out.txt | grep kernel_main
+dd if=kernel.elf of=hello.c32 
+dd if=model.o of=hello.c32 count=1 conv=notrunc
+cp *.c32 CD_root/isolinux/
 genisoimage -o myos.iso -input-charset utf-8 -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4  -boot-info-table ./CD_root 
 
--- emulator.sh
+++ emulator.sh
@@ -1,1 +1,1 @@
-qemu-system-i386 -soundhw pcspk -cdrom myos.iso
+qemu-system-i386 -serial msmouse -cdrom myos.iso
